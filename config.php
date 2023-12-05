d279d28a2a34641a59a40d234afa0972a042fc43
Adrian Maulani <takiya2888157@gmail.com>
2023-12-05 23:54:54 +0700

    Change config to a more comprehensive way

--- config.php
+++ config.php
@@ -1,20 +1,22 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$link = mysqli_connect(
-    $_ENV['DB_HOSTNAME'],
-    $_ENV['DB_USERNAME'],
-    $_ENV['DB_PASSWORD'],
-    $_ENV['DB_DATABASE']
-);
+class Config {
+    public static $link;
 
-if (!$link) {
-    die("Koneksi dengan Database Gagal : " . mysqli_connect_error());
+    public static function initialize() {
+        self::$link = mysqli_connect(
+            $_ENV['DB_HOSTNAME'],
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD'],
+            $_ENV['DB_DATABASE']
+        );
+    }
 }
+
+Config::initialize();
 
 ?>
