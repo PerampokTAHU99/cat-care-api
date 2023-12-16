4dae2bda89492e6f4f84aa221488f3c51490770d
Adrian Maulani <takiya2888157@gmail.com>
2023-12-17 01:39:30 +0700

    Adds comment for config functions and properties

--- config.php
+++ config.php
@@ -5,15 +5,31 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 class Config {
+    /**
+     * Connection object data to be used for every query executions.
+     * @var mysqli
+     */
     public static $link;
 
+    /**
+     * Establish a connection to the database.
+     * This method is autocalled by Composer.
+     * @return void
+     */
     public static function initialize() {
-        self::$link = mysqli_connect(
+        $connect = mysqli_connect(
             $_ENV['DB_HOSTNAME'],
             $_ENV['DB_USERNAME'],
             $_ENV['DB_PASSWORD'],
             $_ENV['DB_DATABASE']
         );
+
+        if (!$connect) {
+            echo mysqli_connect_error();
+        }
+        else {
+            self::$link = $connect;
+        }
     }
 }
 
