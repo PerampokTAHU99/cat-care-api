eb1259e5c56c02f8bf09e3c751705458c0136f94
Adrian Maulani <takiya2888157@gmail.com>
2023-12-17 01:39:50 +0700

    Adds users routes and used helpers for error reponse

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once 'functions/logout.php';
 require_once 'routes/diagnoses.routes.php';
 require_once 'routes/diseases.routes.php';
 require_once 'routes/symptoms.routes.php';
+require_once 'routes/users.routes.php';
 
 use FastRoute\RouteCollector;
 use FastRoute\Dispatcher;
@@ -23,6 +24,8 @@ $dispatcher = simpleDispatcher(function(RouteCollector $route) {
         $r->addGroup('/diseases', 'diseasesRoutes');
 
         $r->addGroup('/symptoms', 'symptomsRoutes');
+
+        $r->addGroup('/users', 'usersRoutes');
     });
 });
 
@@ -33,24 +36,16 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
 switch ($routeInfo[0]) {
     case Dispatcher::NOT_FOUND:
-        header('Content-Type: application/json', true, 404);
-        echo json_encode(
-            array(
-                'status' => 404,
-                'message' => 'Endpoint API tidak ditemukan.'
-            )
-        );
+        Response::error("Endpoint API tidak ditemukan.", 404);
         break;
     case Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
-        header('Content-Type: application/json', true, 405);
-        echo json_encode(
-            array(
-                'status' => 405,
-                'message' => "Method {$httpMethod} tidak diperbolehkan untuk Endpoint ini.",
-                'allowed_method' => $allowedMethods
-            )
-        );
+
+        Response::custom([
+            'status' => 405,
+            'message' => "Method {$httpMethod} tidak diperbolehkan untuk Endpoint ini.",
+            'allowed_method' => $allowedMethods
+        ], 405);
         break;
     case Dispatcher::FOUND:
         $handler = $routeInfo[1];
@@ -60,13 +55,23 @@ switch ($routeInfo[0]) {
             call_user_func($handler, $params);
         }
         catch (TypeError $e) {
-            header('Content-Type: application/json', true, 500);
-            echo json_encode(
-                array(
+            Response::custom(
+                [
                     'status' => 500,
                     'message' => "Koneksi ke Database GAGAL (Configuration Error).",
-                    'debug' => "{$e}" # TODO: Implement Debugging Mode Response
-                )
+                    'debug' => "{$e}"
+                ],
+                500
+            );
+        }
+        catch (mysqli_sql_exception $e) {
+            Response::custom(
+                [
+                    'status' => 500,
+                    'message' => "SQL Query Gagal atau Kolom/Params tidak terisi (SQL Error).",
+                    'debug' => "{$e}"
+                ],
+                500
             );
         }
         break;
